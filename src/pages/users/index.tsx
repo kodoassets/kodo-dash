579f4498a2384e1c714d23b6c3c972faa9798201
Brandow Buenos <brandowbuenos@gmail.com>
2023-07-20 15:59:24 -0300

    feat: add users per country

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -23,6 +23,7 @@ export default function Home() {
         .then((res) => res.data),
   });
 
+  console.log(data);
   const { data: stats } = useQuery(["other"], {
     queryFn: () =>
       axios
@@ -36,29 +37,6 @@ export default function Home() {
   });
 
   if (!stats) return null;
-
-  const usersPerToken = [
-    {
-      token: "KODO1",
-      users: 1000,
-      percentageOfTotal: 50,
-    },
-    {
-      token: "KODO2",
-      users: 500,
-      percentageOfTotal: 25,
-    },
-    {
-      token: "KODO3",
-      users: 250,
-      percentageOfTotal: 12.5,
-    },
-    {
-      token: "KODO4",
-      users: 250,
-      percentageOfTotal: 12.5,
-    },
-  ];
 
   const getRandomColor = () => {
     const letters = "0123456789ABCDEF";
@@ -100,45 +78,54 @@ export default function Home() {
           />
         </div>
 
-        <div className="">
+        <div className="grid grid-cols-3">
           <div className="max-w-[320px] text-center">
             <p className="text-white font-light text-sm mb-2">
               Users per Token
             </p>
-            <div className="bg-gradient-2 rounded-lg p-4">
-              {usersPerToken.map((data) => (
-                <div key={data.token} className="flex flex-row mt-2">
-                  <span className="text-white bg-[#00AEEF] rounded-lg p-1 mr-2 text-xs">
-                    {data.token}
+            <div className="bg-gradient-2 rounded-lg p-4 ">
+              {data.usersPerToken.map((Data) => (
+                <div key={Data.propertyId} className="flex flex-row mt-2">
+                  <span className="text-white bg-[#00AEEF] rounded-lg p-1 mr-2 text-xs min-w-[80px]">
+                    {Data.tokenSymbol}
                   </span>
                   <ProgressBar
-                    progress={data.percentageOfTotal}
-                    innerLabel={data.users.toString()}
+                    progress={
+                      +(
+                        (Data.userWalletCount * 100) /
+                        data.activeUsers
+                      ).toFixed(0)
+                    }
+                    innerLabel={Data.userWalletCount.toString()}
                   />
                   <span className="text-[#7896A1] bg-[#000F14] rounded-lg p-1 ml-2 min-w-[40px] text-xs">
-                    {data.percentageOfTotal}%
+                    {((Data.userWalletCount * 100) / data.activeUsers).toFixed(
+                      0
+                    )}
+                    %
                   </span>
                 </div>
               ))}
             </div>
           </div>
-
-          <div className="max-w-[320px] text-center mt-4">
+          <div className="max-w-[320px] text-center">
             <p className="text-white font-light text-sm mb-2">
               Users per Country
             </p>
-            <div className="bg-gradient-2 rounded-lg p-4">
-              {usersPerToken.map((data) => (
-                <div key={data.token} className="flex flex-row mt-2">
-                  <span className="text-white bg-[#00AEEF] rounded-lg p-1 mr-2 text-xs">
-                    {data.token}
+            <div className="bg-gradient-2 rounded-lg p-4 ">
+              {data.usersPerCountry.map((Data) => (
+                <div key={Data._id} className="flex flex-row mt-2">
+                  <span className="text-white bg-[#00AEEF] rounded-lg p-1 mr-2 text-xs min-w-[80px]">
+                    {Data._id}
                   </span>
                   <ProgressBar
-                    progress={data.percentageOfTotal}
-                    innerLabel={data.users.toString()}
+                    progress={
+                      +((Data.count * 100) / data.activeUsers).toFixed(0)
+                    }
+                    innerLabel={Data.count.toString()}
                   />
                   <span className="text-[#7896A1] bg-[#000F14] rounded-lg p-1 ml-2 min-w-[40px] text-xs">
-                    {data.percentageOfTotal}%
+                    {((Data.count * 100) / data.activeUsers).toFixed(0)}%
                   </span>
                 </div>
               ))}
